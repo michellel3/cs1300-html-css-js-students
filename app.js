eecf371cd9f939044584c178c26b2b739055fcf5
michellel3 <michelle_r_lu@brown.edu>
2022-09-10 17:41:42 -0400

    finished

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -44,11 +44,18 @@ const updatePage = async () => {
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const filtered = fruitsArray.filter(fruit => {
+    return fruit.nutritions.sugar > 15
+  })
 
   // TODO: Create a new HTML element to display your data 
+  const ele = filtered.map(fruit => {
+    const newElement = document.createElement('p');
+    newElement.innerHTML = fruit.name;
+    gallery.append(newElement);
+  })
 
   // TODO: Append your new element to the page
-
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
